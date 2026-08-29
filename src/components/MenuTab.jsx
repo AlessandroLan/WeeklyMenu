f10e365546684cdaeb0690f81656a52e80813c21
AlessandroLan <alessandrolanzo1@gmail.com>
2026-08-29 12:38:23 +0200

    Including notes and copy last week feature

--- src/components/MenuTab.jsx
+++ src/components/MenuTab.jsx
@@ -1,13 +1,18 @@
+import { Copy } from "lucide-react";
 import { DAY_KEYS, dateForDayIndex } from "../lib/dates";
 import { DAY_LABELS } from "../lib/i18n";
 import { useLanguage } from "../lib/LanguageContext";
 import DayCard from "./DayCard";
 
-export default function MenuTab({ monday, menu, onSave }) {
-  const { lang } = useLanguage();
+export default function MenuTab({ monday, menu, onSave, onDuplicate }) {
+  const { lang, t } = useLanguage();
 
   return (
     <div className="menu-tab">
+      <button className="menu-tab__duplicate" onClick={onDuplicate}>
+        <Copy size={14} strokeWidth={2.2} />
+        {t("duplicateWeekButton")}
+      </button>
       {DAY_KEYS.map((dayKey, index) => (
         <DayCard
           key={dayKey}
